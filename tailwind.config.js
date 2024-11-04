26ce98ed22cb537fbae6bbc7350bf1b2c440d9c1
yusran <yyussrraann1992@gmail.com>
2024-11-04 13:36:51 +0800

    responsive1

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      screens: { xxs: "362px" },
+      screens: { xxs: "400px" },
     },
   },
   plugins: [],
