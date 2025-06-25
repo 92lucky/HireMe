3601169a2c18b18d98cb60ecec0293ab2e7a3341
LuckyCode <yyussrraann1992@gmail.com>
2025-06-25 22:48:00 +0800

    add skill set

--- src/components/ShowCase.jsx
+++ src/components/ShowCase.jsx
@@ -7,7 +7,7 @@ const ShowCase = () => {
       <h2 className="mb-3">Skill Set</h2>
 
       <div className="xs:flex  grid grid-cols-1  xs:mt-auto mb-5  xs:gap-10 ">
-        <span className="text-sm text-gray-400">2025-present</span>
+        <span className="text-sm text-gray-400">2025-Update</span>
 
         <div>
           <div className="text-sm font-semibold hover:underline">
@@ -29,6 +29,33 @@ const ShowCase = () => {
           <p className="text-gray-400 text-sm">
             Restfull Api & Database Integration. <br />
             Gorm • GIn • Postgres
+          </p>
+        </div>
+      </div>
+
+      <div className="xs:flex  grid grid-cols-1  xs:mt-auto mb-5  xs:gap-10 ">
+        <span className="text-sm text-gray-400">2025-Update</span>
+
+        <div>
+          <div className="text-sm font-semibold hover:underline">
+            <a
+              className="flex"
+              href="http://github.com/Yusran-Code/Authorization"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Golang Back End{" "}
+              <FiExternalLink
+                style={{
+                  marginTop: "2px",
+                  marginLeft: "2px",
+                }}
+              />
+            </a>
+          </div>
+          <p className="text-gray-400 text-sm">
+            Authenticazion & Authorization. <br />
+            JWTToken • GIn • Postgres
           </p>
         </div>
       </div>
